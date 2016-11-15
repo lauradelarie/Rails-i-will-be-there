d81385d395990bf4da1cd3734539e6e05d83e283
lauradelar <lrdela.rie@gmail.com>
2016-11-15 19:38:09 +0100

    redirect to member path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to member_url, :notice => "Signed in!"
   end
 
 #destroys a user session when user logs out
