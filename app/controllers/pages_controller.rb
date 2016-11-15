9fec19956a9091691abce4da0220142c176a5476
lauradelar <lrdela.rie@gmail.com>
2016-11-15 19:36:13 +0100

    redirect when user not signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,21 @@
 class PagesController < ApplicationController
+  before_action :member_signed_in?, only: [:member]
 
   def home
+  end
+
+  def member
     current_user = User.first
     oauth_token = current_user.token
     member_id = current_user.uid
     @groups = Meetup.new(oauth_token, member_id).groups
   end
+
+
+  private
+    def member_signed_in?
+      if !current_user
+        redirect_to root_url
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
-
+  get 'member' => 'pages#member'
 
   match "/auth/:provider/callback" => "sessions#create", via: [:get, :post]
   match "/signout" => "sessions#destroy", :as => :signout, via: [:get, :post]
