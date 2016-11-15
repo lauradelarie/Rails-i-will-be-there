c20a265a3b5d8f686c442287d7deb8a4e0987afd
lauradelar <lrdela.rie@gmail.com>
2016-11-15 11:50:41 +0100

    Add Omniauth for Meetup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+
+  match "/auth/:provider/callback" => "sessions#create", via: [:get, :post]
+  match "/signout" => "sessions#destroy", :as => :signout, via: [:get, :post]
 end
