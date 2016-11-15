d066a3b486ee2fb662849281be9bfac96898fc36
lauradelar <lrdela.rie@gmail.com>
2016-11-15 12:14:39 +0100

    Add HTTParty gem

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'coffee-rails', '~> 4.2'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'omniauth-meetup', :git => 'https://github.com/AdnanTheExcellent/omniauth-meetup'
+gem 'httparty'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
 
+#creates a new user user when user logs in through meetup.
   def create
     auth = request.env["omniauth.auth"]
      user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
@@ -7,6 +8,7 @@ class SessionsController < ApplicationController
      redirect_to root_url, :notice => "Signed in!"
   end
 
+#destroys a user session when user logs out
   def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "Signed out!"
