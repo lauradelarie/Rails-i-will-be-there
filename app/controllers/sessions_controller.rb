39aef402953ee80f5d2d51d9f89942b5324b0691
lauradelar <lrdela.rie@gmail.com>
2016-11-16 15:27:14 +0100

    Update user token when signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
+  # after_create :update_user_token
 
 #creates a new user user when user logs in through meetup.
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
+    user.update(token: auth["credentials"]["token"])
     redirect_to member_url, :notice => "Signed in!"
   end
 
@@ -15,4 +17,10 @@ class SessionsController < ApplicationController
 
     redirect_to root_url, :notice => "Signed out!"
   end
+
+
+  # private
+  #   def update_user_token
+  #     User.find_by_provider_and_uid(auth["provider"], auth["uid"]).token.update(auth["credentials"]["token"])
+  #   end
 end
