cfb904336086a4344d5719ed0009ea9a67abf9c0
lauradelar <lrdela.rie@gmail.com>
2016-11-16 14:47:52 +0100

    Add refresh groups with Ajax

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,21 @@ class PagesController < ApplicationController
   def home
   end
 
+  def get_groups
+    current_user = User.first
+    oauth_token = current_user.token
+    member_id = current_user.uid
+    @groups = Meetup.new(oauth_token, member_id).groups
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def member
     current_user = User.first
     oauth_token = current_user.token
     member_id = current_user.uid
+    debugger
     @groups = Meetup.new(oauth_token, member_id).groups
     @recommended_events = Meetup.new(oauth_token, member_id).recommended_events
     @events = Meetup.new(oauth_token, member_id).events
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get 'member' => 'pages#member'
+  get '/get_groups' => 'pages#get_groups'
 
   match "/auth/:provider/callback" => "sessions#create", via: [:get, :post]
   match "/signout" => "sessions#destroy", :as => :signout, via: [:get, :post]
